025d38a1231220bfbeceba5656545ab3e866190b
Ronglong Fang <59674706+Ronglong-Fang@users.noreply.github.com>
2024-05-21 23:02:33 -0400

    Add files via upload

--- Regression_on_MNIST_data/SGDL/analysis.py
+++ Regression_on_MNIST_data/SGDL/analysis.py
@@ -14,51 +14,47 @@ import matplotlib.pyplot as plt
 from singlegrade_dnn_regression import singlegrade_model_forward, rse 
 
 
+def results_analysis_bestvalloss(SGDLfile):
 
-def results_analysis(fullfilename):
-
-    with open(fullfilename, 'rb') as f:
-        history, nn_parameter, opt_parameter = pickle.load(f)     
-    
-
-
-    # print(data["opt"])
-    # print(data["train_X"].shape)
-    # print(data["test_X"].shape)
-    
-    print("###########################################################################")
-    print(fullfilename)
-    print(nn_parameter)
-    print(opt_parameter)
+    with open(SGDLfile, 'rb') as f:
+        history, nn_parameter, opt_parameter = pickle.load(f) 
+        
+    print('train_rse is {}, validation_rses is {}'.format(history['train_rses'][-1], history['validation_rses'][-1]))
     
     
+
+    opt = history["opt"]
+    SGDL_epochs = opt_parameter['epochs']
+    SGDL_time  = history['time'][-1]
+    
+    SGDL_xaxis = np.arange(0, SGDL_epochs) * (SGDL_time / SGDL_epochs)
+    single_validation_costs = history["validation_costs"]
+    single_train_costs = history["train_costs"]
+
+
+    plt.plot(SGDL_xaxis, single_validation_costs[:SGDL_epochs], label='validation loss')
+    plt.plot(SGDL_xaxis, single_train_costs[:SGDL_epochs],  label='train loss')
+    plt.xlabel("Training time [s]")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.title('SGDL: $(\\beta, \kappa) = ({}, {})$'.format(int(opt.AMP_Z), int(opt.NORM_K)))
+    plt.tight_layout()
+    plt.show()
+    
+    
+def test_model(SGDLfile):
+    
+    with open(SGDLfile, 'rb') as f:
+        history, nn_parameter, opt_parameter = pickle.load(f)
+   
+
     opt = history['opt']
     data = get_mnist(opt)
-    print(len(history["parameters"]))
     predict_test_Y, _= singlegrade_model_forward(data['test_X'], nn_parameter["layers_dims"],
                                                  history["parameters"][0], nn_parameter["activation"],
                                                  nn_parameter["sinORrelu"])
     
     
-    
     test_rse = rse(data['test_Y'], predict_test_Y)
     cleantest_rse = rse(data['cleantest_Y'], predict_test_Y)
-    
-    
-    
-
-
-    
-
-
-    print('train_rse is {}, validation_rses is {}'.format(history['train_rses'][-1], history['validation_rses'][-1]))
-    print('test_rse is {}, clean_test_rses is {}'.format(test_rse, cleantest_rse))
-
-    print('the train time is {}'.format(history["time"][-1])) 
-
-
-
-        
-    return
-
-
+    print('SGDL test rse on testing data is {}'.format(cleantest_rse))
--- Regression_on_MNIST_data/SGDL/main_run.py
+++ Regression_on_MNIST_data/SGDL/main_run.py
@@ -1,14 +1,14 @@
 from singlegrade_dnn_main import single_dnn_main 
 
-amp = 0.5                                                           # the parameter beta in \tau_{beta, kappa}, chosen from {0.5, 1, 3}                   
-FREQ = [1, 5, 10, 50]                                               # the parameter kappa in \tau_{beta, kappa}, chosen from {1, 5, 10, 50}
+amp = 1                                                              # the parameter beta in \tau_{beta, kappa}, chosen from {0.5, 1, 3}                   
+FREQ = [5]                                                           # the parameter kappa in \tau_{beta, kappa}, chosen from {1, 5, 10, 50}
 
 SGD = False                                                          # if use stochastic method in Adam, then SGD is 'True' and set minibatch size
                                                                      # if use Full grade in Adam, the SGD is 'False' and set mini_batch size to 'False'
 mini_batch_size = False                                              # minibatch size                                           
 layers_dims = [784, 128, 128, 128, 128, 128, 128, 10]                # this is the structure for SGDL
 #epochs = 2000                                                       # the number training epoch in each grade
-epochs = 5  
+epochs = 30  
 
 MAX_learning_rate = [1e-3]                                           # the maximum learning rate, denote as t_max in the paper                                      
 MIN_learning_rate = [1e-4]                                            # the minimum learning rate, denote as t_min in the paper
