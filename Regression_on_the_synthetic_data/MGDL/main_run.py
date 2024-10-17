b7fe1b57749f50fa45f7bb00f2468840771e26f4
Ronglong Fang <59674706+Ronglong-Fang@users.noreply.github.com>
2024-10-16 22:15:17 -0400

    Update main_run.py

--- Regression_on_the_synthetic_data/MGDL/main_run.py
+++ Regression_on_the_synthetic_data/MGDL/main_run.py
@@ -1,14 +1,19 @@
 from multigrade_dnn_main import multi_grade_dnn 
 
-
-Amptype = 'decrease'                                  # choose the type of amplitude from: constant, decrease, increase, vary
+Amptype = 'decrease'                                   # choose the type of amplitude from: constant, decrease, vary, increase
 SGD = False                                            # if use stochastic method in Adam, then SGD is 'True' and set minibatch size
-                                                      # if use Full grade in Adam, the SGD is 'False' and set mini_batch size to 'False'
-mini_batch = False                                    # minibatch size
-mul_layers_dims = [[1, 256, 256, 1], [256, 256, 256, 1], [256, 256, 256, 1], [256, 256, 256, 1]] # this is the structure for MGDL 
-                             
-#mul_epochs = [30000, 30000, 30000, 30000]             # the number training epoch in each grade
-mul_epochs = [300, 300, 300, 300]  
+                                                       # if use Full grade in Adam, the SGD is 'False' and set mini_batch size to 'False'
+mini_batch = False                                     # minibatch size
+
+####################################structure for setting: constant, decrease, vary################################
+mul_layers_dims = [[1, 256, 256, 1], [256, 256, 256, 1], [256, 256, 256, 1], [256, 256, 256, 1]]                            
+mul_epochs = [30000, 30000, 30000, 30000]              # the number training epoch in each grade
+####################################################################################################################
+
+####################################structure for setting: increase##################################################
+#mul_layers_dims = [[1, 256, 256, 1], [256, 256, 256, 1], [256, 256, 256, 1], [256, 256, 256, 1]. [256, 256, 256, 1]]                            
+#mul_epochs = [30000, 30000, 30000, 30000, 30000]              # the number training epoch in each grade
+####################################################################################################################
 
 MAX_learning_rate = [1e-3]                           # the maximum learning rate, denote as t_max in the paper
 MIN_learning_rate = [1e-4]                           # the minimum learning rate, denote as t_min in the paper
