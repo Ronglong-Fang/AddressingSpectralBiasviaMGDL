680c6db51670fa9177b181ff7ce680afe132a963
Ronglong Fang <59674706+Ronglong-Fang@users.noreply.github.com>
2024-10-16 22:52:00 -0400

    Add files via upload

--- Regression_on_the_synthetic_data/SGDL/main_run.py
+++ Regression_on_the_synthetic_data/SGDL/main_run.py
@@ -1,12 +1,20 @@
 from singlegrade_dnn_main import single_dnn_main 
 
-Amptype = "decrease"                                              # choose the type of amplitude from: constant, decrease, increase, vary
+Amptype = "decrease"                                              # choose the type of amplitude from: constant, decrease, vary, increase
 SGD = False                                                       # if use stochastic method in Adam, then SGD is 'True' and set minibatch size
                                                                   # if use Full grade in Adam, the SGD is 'False' and set mini_batch size to 'False'
-mini_batch_size = False                                           # minibatch size                                
-layers_dims = [1, 256, 256, 256, 256, 256, 256, 256, 256, 1]      # this is the structure for SGDL
-#epochs = 30000                                                    # the number of training epoch 
-epochs = 300
+mini_batch_size = False                                           # minibatch size  
+
+######################structure for settings: constant, decrease, vary######################
+layers_dims = [1, 256, 256, 256, 256, 256, 256, 256, 256, 1]     
+############################################################################################
+
+################################structure for settings: increase############################
+#layers_dims = [1, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 1]
+############################################################################################
+
+epochs = 30000                                                    # the number of training epoch 
+
 
 MAX_learning_rate = [1e-3]                                       # the maximum learning rate, denote as t_max in the paper
 MIN_learning_rate = [1e-4]                                       # the minimum learning rate, denote as t_min in the paper
